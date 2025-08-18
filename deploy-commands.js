56088a527416343f561f68a458b24ee84b03a93c
alterGolden <whittylord@gmail.com>
2025-08-18 19:40:00 +0700

    on hold fixing 1

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,8 +1,10 @@
 const { REST, Routes } = require('discord.js');
 const { clientId, guildId, token } = require('../FumoBOT/MainBOT/config.json');
-const afk = require('./MainBOT/OtherFunCommand/afk');
+// const afk = require('./MainBOT/OtherFunCommand/afk');
+// const anime = require('./MainBOT/OtherFunCommand/anime');
 
-const commands = [afk.data.toJSON()]; 
+// const commands = [afk.data.toJSON()]; 
+// commands.push(anime.data.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(token);
 
@@ -12,19 +14,14 @@ const rest = new REST({ version: '10' }).setToken(token);
     try {
         console.log('Started refreshing application (/) commands.');
 
-        await rest.put(
-            Routes.applicationGuildCommands(clientId, guildId), // test guild
-            { body: commands }
-        );
-
         // await rest.put(
-        //     Routes.applicationCommands(clientId), // global
+        //     Routes.applicationGuildCommands(clientId, guildId), // test guild
         //     { body: commands }
         // );
 
         // await rest.put(
-        //     Routes.applicationGuildCommands(clientId, guildId),
-        //     { body: [] }
+        //     Routes.applicationCommands(clientId), // global
+        //     { body: commands }
         // );
 
         console.log('Successfully reloaded application (/) commands.');
@@ -32,3 +29,27 @@ const rest = new REST({ version: '10' }).setToken(token);
         console.error(error);
     }
 })();
+
+// (async () => {
+//     try {
+//         console.log('Started clearing guild and global commands.');
+
+//         // Clear guild commands
+//         await rest.put(
+//             Routes.applicationGuildCommands(clientId, guildId),
+//             { body: [] }
+//         );
+//         console.log('✅ Cleared all guild commands.');
+
+//         // Clear global commands
+//         await rest.put(
+//             Routes.applicationCommands(clientId),
+//             { body: [] }
+//         );
+//         console.log('✅ Cleared all global commands.');
+
+//         console.log('Finished clearing all commands.');
+//     } catch (error) {
+//         console.error(error);
+//     }
+// })();
