03eb12864dd4b3b6386c5a1d772c1fc8f49afaec
alterGolden <whittylord@gmail.com>
2025-08-19 16:41:33 +0700

    remake anime.js into slash command. Will fix the remaining bug on this

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,55 +1,49 @@
+const path = require('path');
 const { REST, Routes } = require('discord.js');
 const { clientId, guildId, token } = require('../FumoBOT/MainBOT/config.json');
-// const afk = require('./MainBOT/OtherFunCommand/afk');
-// const anime = require('./MainBOT/OtherFunCommand/anime');
 
-// const commands = [afk.data.toJSON()]; 
-// commands.push(anime.data.toJSON());
+// Collect commands here
+const commands = [];
 
+// Require each command file
+const sayCommand = require('../FumoBOT/MainBOT/BotTrollinCommand(Owner)/say.js');
+const afkCommand = require('../FumoBOT/MainBOT/OtherFunCommand/afk');
+const animeCommand = require('../FumoBOT/MainBOT/OtherFunCommand/anime');
+
+// Push commands if valid
+if ('data' in sayCommand && 'execute' in sayCommand) {
+    commands.push(sayCommand.data.toJSON());
+} else {
+    console.warn('⚠️ say.js is missing "data" or "execute".');
+}
+
+if ('data' in afkCommand && 'execute' in afkCommand) {
+    commands.push(afkCommand.data.toJSON());
+} else {
+    console.warn('⚠️ afk.js is missing "data" or "execute".');
+}
+
+if ('data' in animeCommand && 'execute' in animeCommand) {
+    commands.push(animeCommand.data.toJSON());
+} else {
+    console.warn('⚠️ anime.js is missing "data" or "execute".');
+}
+
+// REST client
 const rest = new REST({ version: '10' }).setToken(token);
 
-// chay node deploy-commands.js de update
-
+// Deploy commands
 (async () => {
     try {
-        console.log('Started refreshing application (/) commands.');
+        console.log('🔄 Started refreshing application (/) commands.');
 
-        // await rest.put(
-        //     Routes.applicationGuildCommands(clientId, guildId), // test guild
-        //     { body: commands }
-        // );
+        await rest.put(
+            Routes.applicationGuildCommands(clientId, guildId), // use guild deploy for testing
+            { body: commands }
+        );
 
-        // await rest.put(
-        //     Routes.applicationCommands(clientId), // global
-        //     { body: commands }
-        // );
-
-        console.log('Successfully reloaded application (/) commands.');
+        console.log('✅ Successfully reloaded application (/) commands.');
     } catch (error) {
         console.error(error);
     }
 })();
-
-// (async () => {
-//     try {
-//         console.log('Started clearing guild and global commands.');
-
-//         // Clear guild commands
-//         await rest.put(
-//             Routes.applicationGuildCommands(clientId, guildId),
-//             { body: [] }
-//         );
-//         console.log('✅ Cleared all guild commands.');
-
-//         // Clear global commands
-//         await rest.put(
-//             Routes.applicationCommands(clientId),
-//             { body: [] }
-//         );
-//         console.log('✅ Cleared all global commands.');
-
-//         console.log('Finished clearing all commands.');
-//     } catch (error) {
-//         console.error(error);
-//     }
-// })();
